e0690806065cec612e1271173637b94bd1868a4f
Roman Bakaldin <gbakaldin@gmail.com>
2022-01-27 14:58:48 +0300

    Update dependencies and use new features

--- src/main/java/com/github/romanqed/myserver/Server.java
+++ src/main/java/com/github/romanqed/myserver/Server.java
@@ -2,16 +2,15 @@ package com.github.romanqed.myserver;
 
 import io.github.amayaframework.core.AmayaBuilder;
 import io.github.amayaframework.core.AmayaServer;
-import io.github.amayaframework.gson.GsonPipelineConfigurator;
+import io.github.amayaframework.gson.GsonConfigurator;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         AmayaServer server = new AmayaBuilder().
-                bind(new InetSocketAddress(8080)).
-                addConfigurator(new GsonPipelineConfigurator()).
+                bind(8080).
+                addConfigurator(new GsonConfigurator()).
                 build();
         server.start();
     }
