ac3d5d55016833d6df72333bd54c2f2924a7ab48
Roman Bakaldin <gbakaldin@gmail.com>
2022-01-30 20:13:26 +0300

    Update dependencies Update main for new features

--- example-sun/src/main/java/io/github/amayaframework/example/SunServer.java
+++ example-sun/src/main/java/io/github/amayaframework/example/SunServer.java
@@ -1,9 +1,9 @@
 package io.github.amayaframework.example;
 
 import io.github.amayaframework.core.AmayaBuilder;
-import io.github.amayaframework.core.AmayaServer;
 import io.github.amayaframework.core.util.AmayaConfig;
 import io.github.amayaframework.gson.GsonConfigurator;
+import io.github.amayaframework.server.interfaces.HttpServer;
 import org.apache.log4j.BasicConfigurator;
 
 import java.io.IOException;
@@ -12,7 +12,7 @@ public class SunServer {
     public static void main(String[] args) throws IOException {
         BasicConfigurator.configure();
         AmayaConfig.INSTANCE.setDebug(false);
-        AmayaServer server = new AmayaBuilder().
+        HttpServer server = new AmayaBuilder().
                 bind(8080).
                 addConfigurator(new GsonConfigurator(false)).
                 build();
